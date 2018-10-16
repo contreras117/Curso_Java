28ad8f5a89a0a7aad7225c075284a2795d145691
Daniel Contreras <daniel_contreras117@hotmail.com>
2018-10-16 00:25:19 -0600

    Metodos

--- JavaSE/AmazonViewer/AmazonViewer/src/com/contreras/daniel/amazonviewer/Main.java
+++ JavaSE/AmazonViewer/AmazonViewer/src/com/contreras/daniel/amazonviewer/Main.java
@@ -1,26 +1,100 @@
 
 package com.contreras.daniel.amazonviewer;
 
-public class Main
-{
+import java.util.Date;
 
-    public static void main(String[] args)
-    {
-        // TODO Auto-generated method stub
+public class Main {
 
-        int exit = 0;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		showMenu();
 
-        do
-        {
-            System.out.println("BIENVENIDOS A AMAZON VIEWER");
-            System.out.println("\nSelecciona el numero de la opcion deseada");
-            System.out.println("1. Movies");
-            System.out.println("2. Series");
-            System.out.println("3. Books");
-            System.out.println("4. Magazines");
-            System.out.println("0. Exit");
-        }
-        while (exit != 0);
-    }
+	}
 
+	private static void showMenu() {
+		int exit = 0;
+
+		do {
+			System.out.println("BIENVENIDOS A AMAZON VIEWER");
+			System.out.println("\nSelecciona el numero de la opcion deseada");
+			System.out.println("1. Movies");
+			System.out.println("2. Series");
+			System.out.println("3. Books");
+			System.out.println("4. Magazines");
+			System.out.println("5. Report");
+			System.out.println("6. Today Report");
+			System.out.println("0. Exit");
+
+			int response = 0;
+
+			switch (response) {
+			case 0:
+				break;
+			case 1:
+				showMovies();
+				break;
+			case 2:
+				showSeries();
+				break;
+			case 3:
+				showBooks();
+				break;
+			case 4:
+				showMagazines();
+				break;
+			case 5:
+				makeReport();
+				break;
+			case 6:
+				makeReport(new Date());
+				break;
+			default:
+				System.out.println("\nSelecciona una opcion!\n");
+
+			}
+		} while (exit != 0);
+	}
+
+	private static void showMovies() {
+		int response = 1;
+		do {
+			System.out.println("\n:: Movies  ::\n");
+		} while (response != 0);
+	}
+
+	private static void showSeries() {
+		int response = 1;
+		do {
+			System.out.println("\n:: Series  ::\n");
+		} while (response != 0);
+	}
+
+	private static void showChapters() {
+		int response = 1;
+		do {
+			System.out.println("\n:: Chapters  ::\n");
+		} while (response != 0);
+	}
+
+	private static void showBooks() {
+		int response = 1;
+		do {
+			System.out.println("\n:: Books  ::\n");
+		} while (response != 0);
+	}
+
+	private static void showMagazines() {
+		int response = 1;
+		do {
+			System.out.println("\n:: Magazines  ::\n");
+		} while (response != 0);
+	}
+
+	private static void makeReport() {
+
+	}
+
+	private static void makeReport(Date today) {
+
+	}
 }
