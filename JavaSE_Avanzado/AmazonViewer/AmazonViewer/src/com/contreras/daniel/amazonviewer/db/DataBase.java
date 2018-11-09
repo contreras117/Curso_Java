16772cb7f8cfca4dadd08e353a64c7bf5beacaf4
Daniel Contreras <dcontrer@pros.com>
2018-11-08 17:31:30 -0700

    added DB classes

---
+++ JavaSE_Avanzado/AmazonViewer/AmazonViewer/src/com/contreras/daniel/amazonviewer/db/DataBase.java
@@ -0,0 +1,33 @@
+package com.contreras.daniel.amazonviewer.db;
+
+public class DataBase {
+	
+	public static final String URL 		= "jdbc:mysql://localhost:3306";
+	public static final String DB 		= "amazonviewer";
+	public static final String USER 	= "amazonviewer";
+	public static final String PASSWORD = "amazonviewer";
+	
+	public static final String MATERIALT 		= "material";
+	public static final String MATERIALT_ID 	= "id";
+	public static final String MATERIALT_NAME 	= "name";
+	
+	public static final String USERT 		= "user";
+	public static final String USERT_ID 	= "id";
+	public static final String USERT_NAME 	= "name";
+	
+	public static final String VIEWEDT 				= "viewed";
+	public static final String VIEWEDT_IDMATERIAL 	= "id_material";
+	public static final String VIEWEDT_IDELEMENT 	= "id_element";
+	public static final String VIEWEDT_IDUSER 		= "id_user";
+	
+	
+	public static final String MOVIET 			= "movie";
+	public static final String MOVIET_ID 		= "id";
+	public static final String MOVIET_TITLE 	= "title";
+	public static final String MOVIET_GENRE 	= "genre";
+	public static final String MOVIET_DIRECTOR 	= "director";
+	public static final String MOVIET_DURATION 	= "duration";
+	public static final String MOVIET_YEAR 		= "year";
+	
+	
+}
